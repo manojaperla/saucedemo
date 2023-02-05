3046c03939cfa6b3b7f3fa37319aa7dac83d37d4
manoja <manoja.perla@gmail.com>
2023-02-05 14:37:01 -0500

    Modified TestDataManagement.

--- src/main/java/com/saucedemo/qa/data/TestDataMangement.java
+++ src/main/java/com/saucedemo/qa/data/TestDataMangement.java
@@ -1,10 +1,33 @@
 package com.saucedemo.qa.data;
 
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.*;
 public class TestDataMangement {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		FileInputStream file=new FileInputStream("C:\\Users\\sivab\\git\\"
+				+ "saucedemo\\saucedemo\\src\\test\\resources\\TestData\\TestData.xlsx");
 
-	}
+		XSSFWorkbook workbook=new XSSFWorkbook(file);
+		XSSFSheet sheet=workbook.getSheet("sauce");//with Sheet No
+	 // XSSFSheet sheet=workbook.getSheetAt(0); //with sheet index
+		int rowcount=sheet.getLastRowNum();
+    	int colcount =sheet.getRow(0).getLastCellNum();
+    	System.out.println(rowcount);
+    	System.out.println(colcount);
+    	
+    	for(int i=0;i<=rowcount;i++)
+    	{
+    		XSSFRow currentrow=sheet.getRow(i);
+    	
+    		for(int j=0;j<colcount;j++)
+    		{
+    		    System.out.print(currentrow.getCell(j).getStringCellValue()+"|| ");
+    		}
+    		System.out.println();
+    	}
+		 
+		}
 
 }
